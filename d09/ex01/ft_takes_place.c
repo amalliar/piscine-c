64a84652088535f1f43292e310725f7e9de29806
Alexander Chetverkin <virtual-ins@mail.ru>
2020-04-26 09:08:21 +0300

    Fix spaces at the end of line.

--- d09/ex01/ft_takes_place.c
+++ d09/ex01/ft_takes_place.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 17:27:33 by amalliar          #+#    #+#             */
-/*   Updated: 2020/02/06 17:27:43 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/04/26 09:07:57 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_takes_place(int hour)
 {
 	printf("THE FOLLOWING TAKES PLACE BETWEEN %d.00 %c.M. AND %d.00 %c.M\n",
-		(hour % 12) ? hour % 12 : 12, 
+		(hour % 12) ? hour % 12 : 12,
 		(hour % 24 < 12) ? 'A' : 'P',
 		((hour + 1) % 12) ? (hour + 1) % 12 : 12,
 		((hour + 1) % 24 < 12) ? 'A' : 'P');
