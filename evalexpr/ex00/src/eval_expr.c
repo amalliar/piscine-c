2376ee4a8e27fc2df3ed4acb9250f9004dca045d
Alexander Chetverkin <virtual-ins@mail.ru>
2020-04-28 16:20:53 +0300

    Fix minor memory leak.

--- evalexpr/ex00/src/eval_expr.c
+++ evalexpr/ex00/src/eval_expr.c
@@ -6,7 +6,7 @@
 /*   By: amalliar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 07:16:20 by amalliar          #+#    #+#             */
-/*   Updated: 2020/02/15 14:20:56 by amalliar         ###   ########.fr       */
+/*   Updated: 2020/04/28 16:17:18 by amalliar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int				eval_expr(char *str)
 {
 	t_parser	self;
 	size_t		str_sz;
+	int			result;
 
 	if (ft_isempty(str))
 		return (0);
@@ -30,5 +31,7 @@ int				eval_expr(char *str)
 	self.string = (char *)malloc((str_sz + 1) * sizeof(char));
 	ft_memcpy(self.string, str, str_sz + 1);
 	self.index = 0;
-	return (ft_parse_expression(&self));
+	result = ft_parse_expression(&self);
+	free(self.string);
+	return (result);
 }
